45170e1aeca047c7800ed7b68587c82e2bf409d0
Ben Romdhane <hopscorporation@gmail.com>
2017-10-20 23:48:46 +0200

    update server.js && index.html

--- server.js
+++ server.js
@@ -2,21 +2,25 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+var ipaddress   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
-console.log("Server is working PORT=[ "+port+" ] !!!");
-server.listen(port);
+console.log("Server is working I [ "+ipaddress+":"+port+" ] !!!");
+server.listen(port,ipaddress);
 
 app.use(function (req, res, next) {
 	
    console.log("New Request Income !!!");
-    var origin = req.headers.origin;  if(origin == undefined){ return next(); }
-	console.log('"NEW CLIENT FROM : '+origin);
+    var origin = req.headers.origin;  
 
+     if(origin !== undefined){ 
+     console.log('"NEW CLIENT FROM : '+origin);
     res.setHeader('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Methods', 'GET, OPTIONS, POST, HEAD , PUT ,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', true);
+	}
+	
     return next();
 });
 
